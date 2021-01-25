8e446ce147db8f61b2e36f761d75957e846c9557
Nicolas Pellerin <nickypellerin@gmail.com>
2021-01-25 18:15:04 -0500

    Fix message contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -6,16 +6,19 @@ export default async (req, res) => {
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
     
-    const msg = {
-        to: process.env.SENDGRID_EMAIL,
-        from: process.env.SENDGRID_EMAIL,
-        subject,
-        html: `<div>
-            <h1>${name} - ${email}</h1>
-            <h2>${subject}</h2>
-            <p>${message}</p>
-        </div>`
-    }
+    const html = `
+    <h2>${name}</h2>
+    <h4>${email}</h4>
+    <h4>${subject}</h4>
+    <p>${message}</p>
+  `
+
+  const msg = {
+    to: process.env.SEND_GRID_EMAIL,
+    from: process.env.SEND_GRID_EMAIL,
+    subject: `Contact form - LearnFramerMotion: ${subject}`,
+    html: html,
+  }
 
     try {
         await sgMail.send(msg)
