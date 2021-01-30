a0eb853540d491dc4f4aab66108edce408f7950d
Nicolas Pellerin <nickypellerin@gmail.com>
2021-01-30 10:57:55 -0500

    Fix share buttons styling mobile

--- components/Share.tsx
+++ components/Share.tsx
@@ -76,12 +76,12 @@ const Facebook = styled(FacebookIcon)`
   border-radius: 50%;
   width: 4.5rem;
   height: 4.5rem;
-  margin-right: 1.5rem;
-  box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.2);
+  margin-right: 1.8rem;
 
   @media (min-width: 768px) {
     margin-right: 0;
     margin-bottom: 1.2rem;
+    box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.2);
   }
 `
 
@@ -89,7 +89,10 @@ const Twitter = styled(TwitterIcon)`
   border-radius: 50%;
   width: 4.5rem;
   height: 4.5rem;
-  box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.2);
+
+  @media (min-width: 768px) {
+    box-shadow: 0 0 15px 3px rgba(89, 86, 213, 0.2);
+  }
 `
 
 const ShareText = styled.span`
