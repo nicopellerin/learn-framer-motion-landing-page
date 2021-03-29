96c10cbad87f397374f94a0c87c2f7a1ab1bc78c
Nicolas Pellerin <nickypellerin@gmail.com>
2021-03-29 18:55:44 -0400

    Lazy load iframe video demo

--- components/WhatYouWillLearn.tsx
+++ components/WhatYouWillLearn.tsx
@@ -62,6 +62,7 @@ const WhatYouWillLearn = () => {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
+            loading="lazy"
           ></Video>
         </VideoWrapper>
         <VideoText>
